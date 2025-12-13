219e2963869cee5a9a5bdaf7a2fcbdbfcffcbd50
neeboo80 <neebs80@gmail.com>
2025-12-13 02:06:10 -0800

    login

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,54 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/navbar";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {Link, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
 
-export function meta({}: Route.MetaArgs) {
+
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Docugrade" },
+    { name: "description", content: "Intelligent Document validation with grading!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+
+    const { auth } = usePuterStore();
+    
+    const navigate=useNavigate();
+
+    useEffect(() => {
+        if (!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
+
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+  
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track your Graded Documents</h1>
+        <h2>See how your documents are graded and get feedback</h2>
+      </div>
+
+
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+        {
+          resumes.map((resume) => (
+            
+            <ResumeCard key={resume.id} resume={resume} />
+            
+          ))}
+      </div>
+    )}
+
+    </section>
+  </main>
 }
+
